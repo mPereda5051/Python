4b4e0a4d96144529ae3819923dcca427b0af6e54
Miguel <mpereda5051@gmail.com>
2025-11-19 12:42:59 +0100

    alum1=Alumno("Pepe") jkhzxckjhvjkxzhv

--- Tema 4/eje3Alumno.py
+++ Tema 4/eje3Alumno.py
@@ -1,5 +1,6 @@
 class Alumno():
     contador=0
+    nombre = "Los mariachis"
     def __init__(self, nombre=""):
         self.nombre = nombre
         Alumno.contador+=1
@@ -11,3 +12,4 @@ alum2=Alumno("Luis")
 alum3=Alumno("Jaime")
 print("¿Cuantos alumnos hay? ", Alumno.contador)
 print("¿Como se llama?", alum1.nombre)
+print("Nombre clase", Alumno.nombre)
